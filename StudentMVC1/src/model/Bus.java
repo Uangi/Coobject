1b3783fe5b6d78a5325b8b2973990154284e78d0
itwill <itwill@DESKTOP-P4KF3IV>
2023-11-16 19:44:27 +0900

    ㄴㄴ

--- StudentMVC1/src/model/Bus.java
+++ StudentMVC1/src/model/Bus.java
@@ -3,33 +3,6 @@ package model;
 public class Bus {
 
 	// 버스 요금, 수입, 승객 수
-	private final int busCost;	
-	private int busIncome;
-	private int busPassenger;
-	
-	public Bus(int busCost) {
-		this.busCost = busCost;
-	}
-	
-	public int getBusCost() {
-		return busCost;
-	}
-	
-	public int getBusIncome() {
-		return busIncome;
-	}
-
-	public void setBusIncome(int busIncome) {
-		this.busIncome = busIncome;
-	}
-	
-	public int getBusPassenger() {
-		return busPassenger;
-	}
-
-	public void setBusPassenger(int busPassenger) {
-		this.busPassenger = busPassenger;
-	}
 	
 	
 	
