b5b8bc3edb648a6b0f4e5f4bc28d868ca5601ef5
Uangi <jyp423@naver.com>
2023-11-20 11:43:00 +0900

    sun

--- StudentMVC1/src/pre/sum.java
+++ StudentMVC1/src/pre/sum.java
@@ -7,7 +7,10 @@ public class sum {
 
 	
 	public static void main(String[] args) {
-System.out.println("3");
+System.out.println("1");
+System.out.println("1");
+System.out.println("1");
+System.out.println("1");
 
 	
 	}
