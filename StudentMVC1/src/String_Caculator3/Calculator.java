1d31bdfa7e452993c49f2544b191706da5755345
Uangi <jyp423@naver.com>
2023-11-27 20:49:50 +0900

    문자열 계산기 3

---
+++ StudentMVC1/src/String_Caculator3/Calculator.java
@@ -0,0 +1,23 @@
+package String_Caculator3;
+
+public class Calculator {
+
+	public void Calcu(strSplit sp) {
+		
+		if(sp.str.equals("+")) {
+			
+		}
+		else if(sp.str.equals("-")) {
+			
+		}
+		else if(sp.str.equals("*")) {
+			
+		}
+		else if(sp.str.equals("/")) {
+			
+		}
+		
+		
+		
+	}
+}
